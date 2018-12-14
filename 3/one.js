81e70348bd3b08c9ac8f01e9b488ff8072b1ce7f
Kyle <kyleoneill24@gmail.com>
2018-12-13 23:59:02 -0500

    Finished day three

--- 3/one.js
+++ 3/one.js
@@ -3,7 +3,9 @@ var input = (fs.readFileSync('./3/input.txt', 'utf8')).split('\n')
 
 var overlapInches = 0
 var fabricSheet = {}
+var claimIDs = []
 var poisonedClaims = []
+var overlapFreeID
 
 function main() {
     //draw the fabric sheet
@@ -16,18 +18,28 @@ function main() {
         draw(claim)
     })
     //check fabric sheet
-    for (inch in fabricSheet) {
+    for(inch in fabricSheet) {
         if(fabricSheet[inch].indexOf("x") > -1) {
             overlapInches++
         }
     }
+    //find overlap free claim
+    for(claimID in claimIDs) {
+        if(poisonedClaims.indexOf(claimIDs[claimID]) == -1) {
+            overlapFreeID = claimIDs[claimID]
+            break
+        }
+    }
     // 1273 too high
+    //26 wrong
     console.log(`Inches of Overlap: ${overlapInches}\nOverlap Free Claim ID: ${overlapFreeID}`)
 }
 
 function draw(claim) {
     var positions = claim.position.split(',')
     var size = claim.size.split('x')
+    var isPoisoned = false
+    claimIDs.push(claim.id)
     for(var i = 0; i < parseInt(size[0]); i++) {
         for(var j = 0; j < parseInt(size[1]); j++) {
             var xPos = parseInt(positions[0]) + i
@@ -37,8 +49,14 @@ function draw(claim) {
                 fabricSheet[coord] = claim.id
             }
             else {
+                if(fabricSheet[coord] != "x" && poisonedClaims.indexOf(fabricSheet[coord]) == -1) {
+                    poisonedClaims.push(fabricSheet[coord])
+                }
+                if(!isPoisoned) {
+                    poisonedClaims.push(claim.id)
+                }
                 fabricSheet[coord] = "x"
-                poisonedClaims.push(claim.id)
+                isPoisoned = true
             }
         }
     }
