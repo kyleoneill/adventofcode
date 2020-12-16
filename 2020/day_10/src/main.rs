5eb64a01f8cc8921ce8394e175ddcd40005ba5ca
kyle o'neill <kyleoneill24@gmail.com>
2020-12-16 01:28:36 -0600

    Solved day 10

--- 2020/day_10/src/main.rs
+++ 2020/day_10/src/main.rs
@@ -1,17 +1,29 @@
-use std::time::Instant;
+use std::{cmp::max, time::Instant};
 use std::fs;
 use std::io::BufRead;
 
 fn main() {
     let start = Instant::now();
-    let input = get_input("input.txt");
-    let solution = solve(input);
+    let mut input = get_input("input.txt");
+    input.push(0);
+    input.sort();
+    input.push(input[input.len() - 1] + 3);
+    let solution = solve_part_two(&input);
     println!("Found solution in {} microseconds", start.elapsed().as_micros());
     println!("The solution is {}", solution);
 }
 
-fn solve(mut input: Vec<i32>) -> i32 {
-    input.sort();
+fn solve_part_two(input: &[i32]) -> i64 {
+    let mut cache = vec![0; input.len()];
+    cache[0] = 1;
+    for (i, adapter) in input.iter().enumerate().skip(1) {
+        let start = max(i as isize - 3, 0) as usize;
+        cache[i] = cache[start..i].iter().enumerate().filter_map(|(index, a)| if adapter - input[start + index] <= 3 {Some(a)} else {None}).sum();
+    }
+    cache[cache.len() - 1]
+}
+
+fn solve_part_one(mut input: Vec<i32>) -> i32 {
     input.push(input[input.len() - 1] + 3); //Add the device which is +3 of our highest adapter
     let mut diff_one = 0;
     let mut diff_three = 0;
