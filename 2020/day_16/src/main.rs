e9e24c9c5b5fba4ca35353a8461b2c2ca1abf37f
kyleoneill <kyleoneill24@gmail.com>
2020-12-29 23:42:14 -0600

    Solved day 16 part 2

--- 2020/day_16/src/main.rs
+++ 2020/day_16/src/main.rs
@@ -1,27 +1,6 @@
 use std::{collections::HashMap, time::Instant};
 use std::fs;
-use std::io::BufRead;
 use std::ops::Range;
-
-// use std::{collections::HashSet, io, num::ParseIntError, str::FromStr};
-
-// use problem::{CSV, Input, Problem, solve};
-
-// struct Day16;
-// impl Problem for Day16 {
-//     type Input = Info;
-//     type Part1Output = u32;
-//     type Part2Output = u64;
-//     type Error = ();
-
-//     fn part_1(input: &Self::Input) -> Result<Self::Part1Output, Self::Error> {
-        
-//     }
-
-//     fn part_2(input: &Self::Input) -> Result<Self::Part2Output, Self::Error> {
-        
-//     }
-// }
 
 struct Input {
     ticket_fields: HashMap<String, (Range<i32>, Range<i32>)>,
@@ -29,10 +8,36 @@ struct Input {
     nearby_tickets: Vec<Vec<i32>>
 }
 
-fn main() {
-    solve::<Day16>("input").unwrap();
+impl Input {
+    #[allow(dead_code)]
+    fn print_input(&self) {
+        println!("Ticket Fields:");
+        for (key, value) in self.ticket_fields.iter() {
+            println!("{}: {:?}-{:?}", key, value.0, value.1);
+        }
+        println!("My Ticket: ");
+        for field in &self.my_ticket {
+            print!("{}, ", field);
+        }
+        println!("\nNearby Tickets: ");
+        for ticket in &self.nearby_tickets {
+            println!("\nTicket: ");
+            for field in ticket {
+                print!("{}, ", field);
+            }
+        }
+    }
 }
 
+fn main() {
+    let start = Instant::now();
+    let input = get_input("input.txt");
+    let solution = solve_part_two(input);
+    println!("Found solution in {} microseconds", start.elapsed().as_micros());
+    println!("The solution is {}", solution);
+}
+
+#[allow(dead_code)]
 fn solve_part_one(input: Input) -> i32 {
     let mut invalid_values: Vec<i32> = Vec::new();
     for ticket in input.nearby_tickets {
@@ -52,7 +57,66 @@ fn solve_part_one(input: Input) -> i32 {
     invalid_values.iter().sum()
 }
 
-fn solve_part_two(input: Input) -> i32 {
+fn solve_part_two(input: Input) -> u64 {
+    let valid_tickets = get_valid_tickets(&input);
+    let field_order = get_field_order(&input, valid_tickets);
+    let mut counter: u64 = 0;
+    for (i, field) in field_order.iter().enumerate() {
+        let name = field.keys().next().unwrap();
+        if name.starts_with("depa") {
+            let val = input.my_ticket[i] as u64;
+            if counter == 0 {
+                counter = val;
+            }
+            else {
+                counter *= val;
+            }
+        }
+    }
+    counter
+}
+
+fn get_field_order(input: &Input, valid_tickets: Vec<Vec<i32>>) -> Vec<HashMap<String, (Range<i32>, Range<i32>)>> {
+    let mut field_order: Vec<HashMap<String, (Range<i32>, Range<i32>)>> = Vec::new();
+    for field in 0..valid_tickets[0].len() {
+        let mut potential_fields = input.ticket_fields.clone();
+        let mut invalids: Vec<String> = Vec::new();
+        for ticket in 0..valid_tickets.len() {
+            let value = valid_tickets[ticket][field];
+            for (ticket_field, ranges) in potential_fields.iter() {
+                if !(ranges.0.contains(&value) || ranges.1.contains(&value)) {
+                    if !invalids.contains(&ticket_field) {
+                        invalids.push(ticket_field.clone());
+                    }
+                }
+            }
+        }
+        for val in invalids {
+            potential_fields.remove_entry(&val);
+        }
+        field_order.push(potential_fields);
+    }
+    let mut changed = true;
+    while changed {
+        changed = false;
+        for i in 0..field_order.len() {
+            if field_order[i].len() == 1 {
+                for j in 0..field_order.len() {
+                    if i != j {
+                        let asdf = field_order[i].keys().next().unwrap().clone();
+                        match field_order[j].remove(&asdf) {
+                            Some(_removed_val) => changed = true,
+                            None => ()
+                        }
+                    }
+                }
+            }
+        }
+    }
+    field_order
+}
+
+fn get_valid_tickets(input: &Input) -> Vec<Vec<i32>> {
     let mut valid_tickets: Vec<Vec<i32>> = Vec::new();
     for ticket in &input.nearby_tickets {
         let mut ticket_valid =  true;
@@ -73,48 +137,38 @@ fn solve_part_two(input: Input) -> i32 {
             valid_tickets.push(ticket.clone());
         }
     }
-    let mut field_order: Vec<&str> = Vec::new();
-    for ticket in valid_tickets
-    {
-        
-    }
-    0
+    valid_tickets
 }
 
-fn parse_field_values(input: &Vec<String>) -> Result<HashMap<String, (Range<i32>, Range<i32>)>, &'static str> {
-    if input.len() < 20 {
-        return Err("Invalid input");
+fn parse_field_values(input: &str) -> Result<HashMap<String, (Range<i32>, Range<i32>)>, &'static str> {
+    let split_line: Vec<&str> = input.split("\n").collect();
+    let mut map: HashMap<String, (Range<i32>, Range<i32>)> = HashMap::new();
+    for n in 0..split_line.len() {
+        let split: Vec<&str> = split_line[n].split(":").collect();
+        let range_split: Vec<&str> = split[1].split_whitespace().collect();
+        let lower_range: Vec<&str> = range_split[0].split("-").collect();
+        let higher_range: Vec<&str> = range_split[2].split("-").collect();
+        map.insert(split[0].to_string(), (
+            Range {start: lower_range[0].parse().unwrap(), end: lower_range[1].parse::<i32>().unwrap() + 1},
+            Range {start: higher_range[0].parse().unwrap(), end: higher_range[1].parse::<i32>().unwrap() + 1}
+        ));
     }
-    else {
-        let mut map: HashMap<String, (Range<i32>, Range<i32>)> = HashMap::new();
-        for n in 0..20 {
-            let split: Vec<&str> = input[n].split(":").collect();
-            let range_split: Vec<&str> = split[1].split_whitespace().collect();
-            let lower_range: Vec<&str> = range_split[0].split("-").collect();
-            let higher_range: Vec<&str> = range_split[2].split("-").collect();
-            map.insert(split[0].to_string(), (
-                Range {start: lower_range[0].parse().unwrap(), end: lower_range[1].parse::<i32>().unwrap() + 1},
-                Range {start: higher_range[0].parse().unwrap(), end: higher_range[1].parse::<i32>().unwrap() + 1}
-            ));
-        }
-        Ok(map)
-    }
+    Ok(map)
 }
 
 fn parse_ticket(ticket: &str) -> Vec<i32> {
-    ticket.split(",").map(|n| n.parse().expect("Can't parse ticket")).collect()
+    ticket.split(",").map(|n| n.trim().parse().expect("Can't parse ticket")).collect()
 }
 
 fn get_input(filename: &str) -> Input {
-    let file = fs::File::open(filename).expect("Failed to open input file");
-    let lines: Vec<String> = std::io::BufReader::new(file).lines().map(|l| l.expect("Could not parse line")).collect();
-    let mut other_tickets: Vec<Vec<i32>> = Vec::new();
-    for t in 25..lines.len() {
-        other_tickets.push(parse_ticket(&lines[t]));
-    }
+    let file_text = fs::read_to_string(filename).unwrap();
+    let mut split = file_text.split("\r\n\r\n");
+    let fields = split.next().unwrap();
+    let my_ticket = split.next().unwrap().split("\n").skip(1).next().unwrap();
+    let other_tickets: Vec<Vec<i32>> = split.next().unwrap().split("\n").skip(1).map(|ticket| parse_ticket(ticket)).collect();
     Input {
-        ticket_fields: parse_field_values(&lines).unwrap(),
-        my_ticket: parse_ticket(&lines[22]),
+        ticket_fields: parse_field_values(fields).unwrap(),
+        my_ticket: parse_ticket(my_ticket),
         nearby_tickets: other_tickets
     }
 }
