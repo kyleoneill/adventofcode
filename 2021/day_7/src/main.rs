4281f37df3896edb7cf6ac99198c2b4a1f8f003f
kyle <kyleoneill24@gmail.com>
2021-12-08 10:41:00 -0500

    Added a more efficient way to solve day 7

--- 2021/day_7/src/main.rs
+++ 2021/day_7/src/main.rs
@@ -1,21 +1,29 @@
 use problem::{CSV, Problem, solve};
 use std::collections::HashMap;
 
+/*
+Original solution was to iterate every position between the min and max position
+and within each of those iterations calculate the fuel spent per crab. This works out to be
+ n * m time complexity
+
+New solution is to sort the crab positions, find a median, and then calculate each crabs
+distance to the median.
+ sort = n * log(n)
+ Find distance = n
+ Time complexity = n log(n)
+
+ */
+
+// solve_1 uses the faster nlog(n) method
 fn solve_1(input: &CSV<i32>) -> Option<i32> {
-    let lowest_position = input.values.iter().min().unwrap().clone() as usize;
-    let highest_position = input.values.iter().max().unwrap().clone() as usize;
-    let mut cheapest_fuel = i32::MAX;
-    for n in lowest_position..=highest_position {
-        let mut fuel = 0;
-        for m in input.values.iter() {
-            let difference = n as i32 - m;
-            fuel += difference.abs();
-        }
-        if fuel < cheapest_fuel { cheapest_fuel = fuel }
-    }
-    Some(cheapest_fuel)
+    let mut sorted_crabs = input.values.to_owned();
+    sorted_crabs.sort_unstable();
+    let median = sorted_crabs[sorted_crabs.len() / 2];
+    Some(sorted_crabs.iter().map(|&crab| (median - crab).abs()).sum::<i32>())
 }
 
+// solve_2 uses the slower nm method For the step calculation. It reduces time complexity
+// of the added fuel calculation step throuh memoization
 fn solve_2(input: &CSV<i32>) -> Option<i32> {
     let lowest_position = input.values.iter().min().unwrap().clone() as usize;
     let highest_position = input.values.iter().max().unwrap().clone() as usize;
