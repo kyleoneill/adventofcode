c4848c469417f95d67f79bc083e6869dafb8bd3c
kyleoneill <kyleoneill24@gmail.com>
2023-12-05 19:03:33 -0500

    99.99999999999999% reduction in runtime for 2023 day 5 at the cost of my sanity

--- 2023/day_5/src/main.rs
+++ 2023/day_5/src/main.rs
@@ -7,9 +7,16 @@ struct Map {
     range_length: u64
 }
 
-struct C_Range {
-    start: u64,
-    length: u64
+#[derive(Debug, Clone, Copy)]
+struct CRange {
+    pub start: u64,
+    pub length: u64
+}
+
+impl CRange {
+    pub fn end(&self) -> u64 {
+        self.start + self.length
+    }
 }
 
 impl Map {
@@ -43,16 +50,64 @@ impl Map {
         ret.push(buffer);
         ret
     }
-    pub fn map_value(map: &Vec<Map>, value: u64) -> u64 {
-        for val in map {
-            let end_range = val.source_range + val.range_length;
-            let range = val.source_range..end_range;
-            if range.contains(&value) {
-                let i = value - val.source_range;
-                return val.destination_range + i;
+    pub fn map_value(map: &Vec<Map>, mut value_vec: Vec<CRange>) -> Vec<CRange> {
+        let mut results: Vec<CRange> = Vec::new();
+        for map_value in map {
+            let mut new_value_vec: Vec<CRange> = Vec::new();
+            while let Some(value) = value_vec.pop() {
+                let map_range_end = map_value.source_range + map_value.range_length;
+                let value_end = value.end();
+
+                // The map range completely contains the value range
+                if value.start >= map_value.source_range && value_end <= map_range_end {
+                    let offset = value.start - map_value.source_range;
+                    results.push(CRange { start: map_value.destination_range + offset, length: value.length });
+                }
+
+                // The input start begins before the map range and ends inside the map range
+                else if value.start < map_value.source_range && value_end > map_value.source_range && value_end < map_range_end {
+                    // outside left
+                    let offset = map_value.source_range - value.start;
+                    new_value_vec.push(CRange { start: value.start, length: offset });
+
+                    // inside
+                    let offset = value_end - map_value.source_range;
+                    results.push(CRange { start: map_value.destination_range, length: offset });
+                }
+
+                // The input start begins inside the map range and ends outside the map range
+                else if value.start >= map_value.source_range && value.start < map_range_end && value_end >= map_range_end {
+                    // inside
+                    let offset = map_range_end - value.start;
+                    let distance_into_map = value.start - map_value.source_range;
+                    results.push(CRange { start: map_value.destination_range + distance_into_map, length: offset });
+
+                    // outside right
+                    let offset = value_end - map_range_end;
+                    new_value_vec.push(CRange { start: map_range_end, length: offset });
+                }
+                // The input completely contains the map range and must be broken into a before, during, and after
+                else if map_value.source_range >= value.start && map_range_end <= value_end {
+                    // outside left
+                    let offset = map_value.source_range - value.start;
+                    new_value_vec.push(CRange { start: value.start, length: offset });
+
+                    // inside
+                    let offset = map_range_end - map_value.source_range;
+                    results.push(CRange { start: map_value.destination_range, length: offset });
+
+                    // outside right
+                    let offset = value_end - map_range_end;
+                    new_value_vec.push(CRange { start: map_range_end, length: offset });
+                }
+                else {
+                    new_value_vec.push(value);
+                }
             }
+            value_vec = new_value_vec;
         }
-        value
+        results.append(&mut value_vec);
+        results
     }
 }
 
@@ -60,34 +115,28 @@ impl Map {
 struct Seed;
 
 impl Seed {
-    pub fn find_smallest_location(seed_numbers: Vec<u64>, map_map: Vec<Vec<Map>>) -> u64 {
-        let mut smallest_location = u64::MAX;
-        for (i, seed_number) in seed_numbers.iter().enumerate() {
-            let soil = Map::map_value(&map_map[0], *seed_number);
-            let fertilizer = Map::map_value(&map_map[1], soil);
-            let water = Map::map_value(&map_map[2], fertilizer);
-            let light = Map::map_value(&map_map[3], water);
-            let temperature = Map::map_value(&map_map[4], light);
-            let humidity = Map::map_value(&map_map[5], temperature);
-            let location = Map::map_value(&map_map[6], humidity);
-            if location < smallest_location {
-                smallest_location = location;
-            }
-        }
-        smallest_location
+    pub fn find_smallest_location(seed_numbers: Vec<CRange>, map_map: Vec<Vec<Map>>) -> u64 {
+        let soil = Map::map_value(&map_map[0], seed_numbers);
+        let fertilizer = Map::map_value(&map_map[1], soil);
+        let water = Map::map_value(&map_map[2], fertilizer);
+        let light = Map::map_value(&map_map[3], water);
+        let temperature = Map::map_value(&map_map[4], light);
+        let humidity = Map::map_value(&map_map[5], temperature);
+        let location = Map::map_value(&map_map[6], humidity);
+        location.iter().map(|x| x.start).min().unwrap()
     }
-    pub fn get_seed_numbers(input: &Vec<String>) -> Vec<C_Range> {
-        let mut output: Vec<C_Range> = Vec::new();
+    pub fn get_seed_numbers(input: &Vec<String>) -> Vec<CRange> {
+        let mut output: Vec<CRange> = Vec::new();
         let vals = input[0].split(" ").skip(1);
         for val in vals {
             let as_num = val.parse::<u64>().expect("Failed to convert seed num to u64");
-            let c_range = C_Range { start: as_num, length: 1u64 };
+            let c_range = CRange { start: as_num, length: 1u64 };
             output.push(c_range);
         }
         output
     }
-    pub fn get_seed_ranges(input: &Vec<String>) -> Vec<u64> {
-        let mut output: Vec<u64> = Vec::new();
+    pub fn get_seed_ranges(input: &Vec<String>) -> Vec<CRange> {
+        let mut output: Vec<CRange> = Vec::new();
         let mut buffer: [u64; 2] = [0, 0];
         let vals = input[0].split(" ").skip(1);
         for (i, val) in vals.enumerate() {
@@ -95,10 +144,7 @@ impl Seed {
             let modu = i % 2;
             buffer[modu] = as_num;
             if modu == 1 {
-                let end_range = buffer[0] + buffer[1];
-                for j in buffer[0]..end_range {
-                    output.push(j);
-                }
+                output.push(CRange { start: buffer[0], length: buffer[1] })
             }
         }
         output
@@ -119,9 +165,6 @@ impl Problem for Day5 {
     }
 
     fn solve_part_two(input: &Self::Input) -> Self::PartTwo {
-        // Part 2 has a two minute runtime which is unacceptably long
-        // Better solution: Rather than converting the seed range into seeds ((79 14) into (79, 80, 81, 82...)) it is
-        //  possible to process the seed ranges themselves, drastically cutting down on iterations
         let map_map: Vec<Vec<Map>> = Map::from_input(input);
         let seed_numbers = Seed::get_seed_ranges(input);
         Seed::find_smallest_location(seed_numbers, map_map)
